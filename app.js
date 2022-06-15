089232f56ce8c60a29267b6f62a7f0dc956ca55a
rafonha <johndoe@example.comrafaela.sdp25@gmail.com>
2022-06-15 20:34:53 -0300

    show countries and flags

--- app.js
+++ app.js
@@ -1,20 +1,29 @@
-const fetchCountries= () => {
+const fetchCountries = async () => {
     const url = path => `https://restcountries.com/v2/${path}`
 
-    const countriesPromises = []
+    const countriesArray = []
 
-    for (let i = 1; i < 195; i++) {
-        countriesPromises.push(fetch(url("all")).then(res => res.json()))
-    }
+    countriesArray.push(await fetch(url("all")).then(res => res.json()))
 
-    Promise.all(countriesPromises).then(countries => {
-        console.log(countries)
+    console.log(countriesArray[0])
 
-        const countryLi = countries.reduce((acc, country) => {
-            acc += `<li>${country.name}</li>`
-            return acc
-        }, '')
+    let countriesLi = ''
+
+    countriesArray[0].forEach(country => {
+
+        const countriesEach = `
+        <li class="countryCard ${country.region}">
+            <img class="countryCard__flag" alt="${country.name}" src="${country.flags.png}" />
+            <h3>${country.name}</h3>
+            <p>Capital: ${country.name}</p>
+            <p>Região: ${country.region}</p>
+        </li>
+        `
+        countriesLi += countriesEach
     })
+
+        const ul = document.getElementById('countryList__ul')
+        ul.innerHTML = countriesLi
 }
 
 fetchCountries()
